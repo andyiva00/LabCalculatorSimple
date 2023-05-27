df864ec3697cbc39e129725d47ff29e3a12ea2f1
Andrey Ivanov <andyiva00@gmail.com>
2023-05-27 16:36:08 +0500

    Implemented adding/removing digits of the current operand

--- MainForm.cs
+++ MainForm.cs
@@ -12,6 +12,7 @@ namespace LabCalculatorSimple
 {
     public partial class MainForm : Form
     {
+        string currentOperand = "0";
         private enum MathFunctions
         {
             None, Add, Subtract, Multiply, Divide, Sqr, Sqrt, OverX, Percent
@@ -58,10 +59,89 @@ namespace LabCalculatorSimple
 
         private void button_Click(object sender, EventArgs e)
         {
-            // DEBUG:
-            arithmometer.SetLeftOperand(1.0f);
-            arithmometer.SetRightOperand(2.0f);
+            handler_keypress(((System.Windows.Forms.Control)sender).Name);
         }
-        
+
+        private void handler_keypress(string Name)
+        {
+            switch (Name)
+            {
+                case "buttonZero":
+                    AddDigit('0');
+                    break;
+                case "buttonOne":
+                    AddDigit('1');
+                    break;
+                case "buttonTwo":
+                    AddDigit('2');
+                    break;
+                case "buttonThree":
+                    AddDigit('3');
+                    break;
+                case "buttonFour":
+                    AddDigit('4');
+                    break;
+                case "buttonFive":
+                    AddDigit('5');
+                    break;
+                case "buttonSix":
+                    AddDigit('6');
+                    break;
+                case "buttonSeven":
+                    AddDigit('7');
+                    break;
+                case "buttonEight":
+                    AddDigit('8');
+                    break;
+                case "buttonNine":
+                    AddDigit('9');
+                    break;
+                case "buttonPeriod":
+                    AddPeriod();
+                    break;
+                case "buttonBackspace":
+                    Backspace();
+                    break;
+
+            }
+
+            // DEBUG
+            textBoxOperand.Text = currentOperand;
+        }
+
+        private void AddDigit(char digit)
+        {
+            if (currentOperand == "0")
+            {
+                currentOperand = "";
+            }
+                
+            currentOperand += digit;
+        }
+
+        private void AddPeriod()
+        {
+            if (!currentOperand.Contains(".")) 
+            {
+                currentOperand += '.';
+            }
+        }
+
+        private void Backspace()
+        {
+
+            if (currentOperand.Length > 0)
+            {
+                currentOperand = currentOperand.Remove(currentOperand.Length - 1, 1);
+            }
+
+            if (currentOperand.Length == 0)
+            {
+                currentOperand = "0";
+            }
+            
+        }
+
+
     }
 }
