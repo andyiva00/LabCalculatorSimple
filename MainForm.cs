0c7398337f25634750c82bab46b8384903a20894
Andrey Ivanov <andyiva00@gmail.com>
2023-05-27 15:38:30 +0500

    Calc class prototype prepared

--- MainForm.cs
+++ MainForm.cs
@@ -12,6 +12,45 @@ namespace LabCalculatorSimple
 {
     public partial class MainForm : Form
     {
+        private enum MathFunctions
+        {
+            None, Add, Subtract, Multiply, Divide, Sqr, Sqrt, OverX, Percent
+        }
+
+        class Calc
+        {
+            private float leftOperand = 0f;
+            private float rightOperand = 0f;
+            private bool leftOperandNegativeSign = false;
+            private bool righOperandNegativeSign = false;
+            private float memorySum = 0f;
+            private MathFunctions mathFunctions = MathFunctions.None;
+
+            public Calc() { }
+
+            public void ClearAll()
+            {
+                leftOperand = 0f;
+                rightOperand = 0f;
+                leftOperandNegativeSign = false;
+                righOperandNegativeSign = false;
+                memorySum = 0f;
+                mathFunctions = MathFunctions.None;
+            }
+
+            public void SetLeftOperand(float operand)
+            {
+                leftOperand = operand;
+            }
+
+            public void SetRightOperand(float operand)
+            {
+                rightOperand = operand;
+            }
+        }
+
+        Calc arithmometer = new Calc();
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,7 +58,9 @@ namespace LabCalculatorSimple
 
         private void button_Click(object sender, EventArgs e)
         {
-
+            // DEBUG:
+            arithmometer.SetLeftOperand(1.0f);
+            arithmometer.SetRightOperand(2.0f);
         }
         
     }
